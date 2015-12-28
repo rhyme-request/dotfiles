f401f141211d003ebe0c0bd678fb6b654dfa0daa
dcadmin <dcadmin@MacBook-Pro.local>
2015-12-28 10:17:41 +0900

    commited for add configs

---
+++ README.md
@@ -0,0 +1,7 @@
+
+purpose of this file is setting tmux,zsh, and etc...
+
+do this.
+
+ $ chmod +x ~/dotfiles/deploy.sh
+ $ ~/dotfiles/deploy.sh
---
+++ deploy.sh
@@ -0,0 +1,10 @@
+cd ${DOT_DIRECTORY}
+
+for f in .??*
+do
+  # 無視したいファイルやディレクトリはこんな風に追加してね
+  [[ ${f} = ".git" ]] && continue
+  [[ ${f} = ".gitignore" ]] && continue
+  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+done
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
